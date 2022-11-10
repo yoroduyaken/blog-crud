fb080b061a12f70dcb6f92f31639cee06aec9d18
yoroduyaken <yoroduya.kento@gmail.com>
2022-11-10 21:29:11 +0900

    /blogのページを作成した

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+<body>
+    <h1>Blog Page</h1>
+
+    <textarea name="tarea3" cols="30" rows="3" maxlength="1000" 
+    placeholder="投稿してください。"></textarea>
+</body>
+</html>
