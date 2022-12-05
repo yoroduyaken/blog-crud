d34d0e53c2a7c829cd1fe25676b72d24afc2b161
yoroduyaken <yoroduya.kento@gmail.com>
2022-12-05 22:58:14 +0900

    storeメソッドを追加した。

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Blog;
 
 class BlogController extends Controller
 {
     public function create()
     {
         return view('blog');
+    }
+
+    public function store(Request $request)
+    {
+        $blog = Blog::create([
+            'title' => $request->title,
+        ]);
+
+        return redirect()->route('blog');
     }
 }
