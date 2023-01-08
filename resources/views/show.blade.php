be6c246984c8fe70128dd9c56ee06b5d28d98bde
yoroduyaken <yoroduya.kento@gmail.com>
2023-01-08 12:53:00 +0900

    エラーコードとforeach分が不要の為、削除した。

---
+++ resources/views/show.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>ブログ一覧</title>
+</head>
+<body>
+    <h1>blog crud</h1>
+    <p>マイページ一覧</p>
+
+    <table border="3" cellpadding="6" cellspacing="0">
+        <caption>マイページブログ一覧</caption>
+        <tr>
+            <th>本文</th>
+            <th>作成日</th>
+        </tr>
+        <tr>
+            <td>{{ $blog->body }}</td>
+            <td>{{ $blog->created_at }}</td>
+        </tr>
+    </table>
+
+</body>
+
+</html>
