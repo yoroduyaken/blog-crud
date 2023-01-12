04472fc09658b34e80577f572d8c7af63ad8d4fe
yoroduyaken <yoroduya.kento@gmail.com>
2023-01-12 22:11:09 +0900

    不要なgetを削除した。

--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,4 @@ Route::get('/blogs/{id}', [BlogController::class, 'show'])->name('blogs.show');
 Route::get('/blogs/{id}/edit', [BlogController::class, 'edit'])->name('blogs.edit');
 Route::post('/blogs/{id}/edit', [BlogController::class, 'update']);
 
-Route::get('/blogs/{id}/delete', [BlogController::class, 'index'])->name('blogs.delete');
 Route::post('/blogs/{id}/delete', [BlogController::class, 'destroy']);
