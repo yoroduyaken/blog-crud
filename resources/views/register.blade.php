3645ba8a68eb4ddd4e50c513c6f330298836d4bf
yoroduyaken <yoroduya.kento@gmail.com>
2023-01-16 22:41:30 +0900

    登録画面のページを作成した。

---
+++ resources/views/register.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>登録フォーム</title>
+</head>
+<body>
+    <form action="/register"  method="post">
+        {{ csrf_field() }}
+    <dl>
+        <dt>ユーザー名</dt>
+        <dd><input type="text" name="name" size="30"></dd>
+    </dl>
+    <dl>
+        <dt>メールアドレス</dt>
+        <dd><input type="email" name="email" size="30"></dd>
+    </dl>
+    <dl>
+        <dt>パスワード</dt>
+        <dd><input type="password" name="password" size="30"></dd>
+    </dl>
+    <dl>
+        <dt>パスワード確認</dt>
+        <dd><input type="password" name="password_confirmation" size="30"></dd>
+    </dl>
+        <button type="submit" name='action'>登録</button>
+    </form>
+</body>
+
+</html>
