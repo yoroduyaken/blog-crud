cbd3c536d1560cd1601beab7c11e7cc9c4f6bf6a
yoroduyaken <yoroduya.kento@gmail.com>
2023-01-19 22:09:12 +0900

    ログイン画面のページを作成した。

---
+++ resources/views/login.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>ログインフォーム</title>
+</head>
+<body>
+    <form action="{{ route('login') }}"  method="post">
+        {{ csrf_field() }}
+        <dl>
+            <dt>メールアドレス</dt>
+            <dd><input type="email" name="email" size="30"></dd>
+        </dl>
+        <dl>
+            <dt>パスワード</dt>
+            <dd><input type="password" name="password" size="30"></dd>
+        </dl>
+        <button type="submit" name='action'>ログイン</button>
+    </form>
+</body>
+
+</html>
