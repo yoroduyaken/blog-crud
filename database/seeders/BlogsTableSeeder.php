e31206dbcddc9a644f4ac9995a3952027c69c2f4
yoroduyaken <yoroduya.kento@gmail.com>
2023-02-26 22:49:08 +0900

    Auth Facadesを削除した。

--- database/seeders/BlogsTableSeeder.php
+++ database/seeders/BlogsTableSeeder.php
@@ -6,7 +6,6 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
 
 
 class BlogsTableSeeder extends Seeder
