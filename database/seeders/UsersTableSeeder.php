90095fe953a8bed9f51f1c9a057ce8d8165525d8
yoroduyaken <yoroduya.kento@gmail.com>
2023-03-05 11:03:17 +0900

    Factoryにてユーザーを10人作成した。

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -16,12 +17,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $param = [
-            'name' => 'seederUser',
-            'email' => 'seeder@email.com',
-            'password' => Hash::make('password'),
-            'email_verified_at' => now(),
-        ];
-        DB::table('users')->insert($param);
+        // $param = [
+        //     'name' => 'seederUser',
+        //     'email' => 'seeder@email.com',
+        //     'password' => Hash::make('password'),
+        //     'email_verified_at' => now(),
+        // ];
+        // DB::table('users')->insert($param);
+        User::factory()->count(10)->create();
+        User::factory()->unverified()->count(2)->create();
     }
 }
