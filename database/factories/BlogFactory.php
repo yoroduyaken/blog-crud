1da3ca2dfbc3e1661ac9b4f0ac57c002d1aa71f7
yoroduyaken <yoroduya.kento@gmail.com>
2023-03-08 22:00:39 +0900

    Seeder&Factoryでブログを25件追加した。

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
+ */
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->title(),
+            'body' => fake()->unique()->text(20),
+            'user_id' => User::first()->id,
+        ];
+    }
+}
--- database/seeders/BlogsTableSeeder.php
+++ database/seeders/BlogsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Blog;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,11 +18,12 @@ class BlogsTableSeeder extends Seeder
      */
     public function run()
     {
-        $param = [
-            'title' => '『Seederとは』',
-            'body' => 'Laravelは、シードクラスを使用してデータベースにデータをシード（種をまく、初期値の設定）する機能を持っています。すべてのシードクラスはdatabase/seedersディレクトリに保存します。デフォルトで、DatabaseSeederクラスが定義されています。このクラスから、callメソッドを使用して他のシードクラスを実行し、シードの順序を制御できます。',
-            'user_id' => User::first()->id,
-        ];  
-        DB::table('blogs')->insert($param);
+        // $param = [
+        //     'title' => '『Seederとは』',
+        //     'body' => 'Laravelは、シードクラスを使用してデータベースにデータをシード（種をまく、初期値の設定）する機能を持っています。すべてのシードクラスはdatabase/seedersディレクトリに保存します。デフォルトで、DatabaseSeederクラスが定義されています。このクラスから、callメソッドを使用して他のシードクラスを実行し、シードの順序を制御できます。',
+        //     'user_id' => User::first()->id,
+        // ];  
+        // DB::table('blogs')->insert($param);
+        Blog::factory()->count(25)->create();
     }
 }
